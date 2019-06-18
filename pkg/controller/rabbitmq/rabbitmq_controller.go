e85df48493626f3bbb05460dba81ddc6e72c5ed1
Michael Henkel <mhenkel@juniper.net>
2019-06-17 17:23:06 -0700

    fix

--- pkg/controller/rabbitmq/rabbitmq_controller.go
+++ pkg/controller/rabbitmq/rabbitmq_controller.go
@@ -52,16 +52,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner Rabbitmq
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &contrailv1alpha1.Rabbitmq{},
-	})
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
